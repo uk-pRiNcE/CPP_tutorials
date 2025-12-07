3a238723c6a48a7c82cb38fe71de63f6cf50caf3
Prince Chauhan <prince_ua2504cdh128@iitp.ac.in>
2025-12-07 15:46:37 +0000

    my first cpp problem

---
+++ cpp_problem_1.cpp
@@ -0,0 +1,21 @@
+// problem-1 
+// to create a program to determine weather the integer is an odd or an even no.
+// code:-
+
+#include <iostream> //allows us to use std::cout or std::cin
+using namespace std;
+
+int main() {
+    int N ; //this will store the number entered by the user 
+    cout<<"enter the number:" ; //asking the user 
+    cin>>N ; //storing the value in variable
+
+    //check if the input is even or odd
+    if (N % 2 == 0){
+        cout<<"EVEN" <<endl ;
+    } else {
+        cout<<"ODD" <<endl ;
+    }
+
+    return 0 ; //tell the OS that program ended successfully 
+}
